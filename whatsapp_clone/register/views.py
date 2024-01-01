2e8b84a57d6fcd9b7a0498fc89fd4210989fea10
franchsli <112600427+franchsli@users.noreply.github.com>
2024-01-01 17:07:02 -0500

    Registration system added

--- whatsapp_clone/register/views.py
+++ whatsapp_clone/register/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from .forms import CustomUserCreationForm
+from django.http import HttpResponse
 
 # Create your views here.
 def register(request):
@@ -8,7 +9,17 @@ def register(request):
         'form': CustomUserCreationForm()
     }
     if request.method == 'POST':
-        ...
+        new_user_form = CustomUserCreationForm(data=request.POST)
+        if new_user_form.is_valid():
+            new_user = new_user_form.save()
+            login(request, new_user)
+            return redirect('home')
+        else:
+            return HttpResponse(f'INVALID FORM: {new_user_form.errors}')
+        
+    return render(request, 'registration/register.html', data)
+
+
 
 def exit(request):
     logout(request)
