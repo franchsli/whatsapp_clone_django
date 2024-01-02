7ef6515f6722696992e13babe63b0de7fe5ace0f
franchsli <112600427+franchsli@users.noreply.github.com>
2024-01-02 15:18:06 -0500

    Phonenumber instaled

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Whatsapp clone
+
+TODO:
+    - Custom the clase User to have phonenumber and a list of chats
--- whatsapp_clone/home/models.py
+++ whatsapp_clone/home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class Message(models.Model):
+    from_user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    to_user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    text = models.TextField(blank=False, null=False)
+    date = models.DateTimeField(default=timezone.now)
+
+class Chat(models.Model):
+    from_user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    to_user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    messages = models.ForeignKey(Message, on_delete=models.CASCADE)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    chats = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    phone_number = PhoneNumberField()
