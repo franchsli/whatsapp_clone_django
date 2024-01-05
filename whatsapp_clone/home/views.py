370b1a4493a55a70a0deb780f84040cd065bf02a
franchsli <112600427+franchsli@users.noreply.github.com>
2024-01-05 17:36:57 -0500

    Model vieww sets added

--- whatsapp_clone/home/views.py
+++ whatsapp_clone/home/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from .models import Message, Chat, Profile
+from .serializers import UserSerializer, MessageSerializer, ChatSerializer, ProfileSerializer
+from rest_framework.viewsets import ModelViewSet
 # Create your views here.
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class MessageViewSet(ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+class ChatViewSet(ModelViewSet):
+    queryset = Chat.objects.all()
+    serializer_class = ChatSerializer
+
+class ProfileViewSet(ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
 def home(request):
     return render(request, 'home.html', {})
