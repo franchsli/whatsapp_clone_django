9a76f81e67ed8ec57c7f7e573392049a54d5905f
franchsli <112600427+franchsli@users.noreply.github.com>
2024-01-05 17:19:54 -0500

    Serializers created

---
+++ whatsapp_clone/home/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.models import User
+from .models import Message, Chat, Profile
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+class MessageSerializer(ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
+
+class ChatSerializer(ModelSerializer):
+    class Meta:
+        model = Chat
+        fields = '__all__'
+
+class ProfileSerializer(ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
