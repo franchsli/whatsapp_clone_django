17830c701d50071581c3c1687a84c5ca0d7bc6c1
franchsli <112600427+franchsli@users.noreply.github.com>
2024-01-08 11:11:52 -0500

    Created websocket consumer and routing

--- whatsapp_clone/whatsapp_clone/asgi.py
+++ whatsapp_clone/whatsapp_clone/asgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+import home.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whatsapp_clone.settings')
 
@@ -18,3 +21,16 @@ application = ProtocolTypeRouter(
         "http": get_asgi_application(),
     }
 )
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": get_asgi_application(),
+
+    # WebSocket chat handler
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                home.routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
