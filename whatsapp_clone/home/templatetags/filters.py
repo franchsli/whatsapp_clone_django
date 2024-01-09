c578d8b28162c74585db272c9755b3d0bcdaa060
franchsli <112600427+franchsli@users.noreply.github.com>
2024-01-09 16:51:20 -0500

    Now the chats are displayed with the contact name

---
+++ whatsapp_clone/home/templatetags/filters.py
@@ -0,0 +1,12 @@
+from django import template
+register = template.Library()
+
+@register.filter
+def exclude_user(value, user):
+    #value = value.remove(user)
+    #print(type(user))
+    #print(type(value[1]))
+    #print(value[0])
+    #print(user.username)
+    return value[1].user if value[1].user != user else value[0].user
+ 
