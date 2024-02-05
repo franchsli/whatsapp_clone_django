710862b560b69f79b8dbbf4d3da46af46ac4c7b1
franchsli <112600427+franchsli@users.noreply.github.com>
2024-02-05 16:05:57 -0500

    Last message logic added

--- README.md
+++ README.md
@@ -34,6 +34,7 @@ Also don't forget to change the channel layer to redis.
     the modal form title should change to 'create group' and a new input should appear that has the label
         'group name'.
 - Use for... empty in for loops if necessary. <https://docs.djangoproject.com/en/5.0/ref/templates/builtins/#for-empty>
+- Remove all unnecesary datasets and such from chats, contact templates.
 - Fix: When there's only a contact or chat in the list, its option dropdown seems weird.
 - Make it responsive.
 - Re think the delete message modal (Delete for everyone or for me logic).
--- whatsapp_clone/chat/templatetags/filters.py
+++ whatsapp_clone/chat/templatetags/filters.py
@@ -1,50 +1,60 @@
 from django import template
 from phonenumber_field.phonenumber import PhoneNumber
 from chat.models import Contact, User
+
 register = template.Library()
+
 
 @register.filter
 def exclude_user(value, user):
-    user_list = list(value.values_list('username', flat=True))
+    user_list = list(value.values_list("username", flat=True))
     return user_list[1] if user_list[1] != user.username else user_list[0]
- 
+
+
 @register.filter
 def to_list(value):
     # you CAN ALSO use list comprehension (super effective)
-    #print(value.values_list('sender_user', 'text').order_by('-date'))
-    #list(value.values_list('sender_user', 'text').order_by('-date'))
-    return list(value.values_list('id', flat=True).order_by('date'))
+    # print(value.values_list('sender_user', 'text').order_by('-date'))
+    # list(value.values_list('sender_user', 'text').order_by('-date'))
+    return list(value.values_list("id", flat=True).order_by("date"))
+
 
 @register.filter
 def last_message(value, data):
-    messages_data = list(value.values_list(data, flat=True).order_by('date'))
+    messages_data = list(value.values_list(data, flat=True).order_by("date"))
     return messages_data[-1]
-    
+
 
 @register.simple_tag
 def exclude_user_tag(user_set, user, value):
     user_list = list(user_set.values_list(value, flat=True))
-    comparison = user.username if value == 'username' else user.phone_number
+    comparison = user.username if value == "username" else user.phone_number
     return user_list[1] if user_list[1] != comparison else user_list[0]
+
 
 @register.simple_tag
 def get_contact_in_chat(user_set, auth_user, desired_value):
-    phones_list = list(user_set.values_list('phone_number', flat=True))
-    result = phones_list[0] if auth_user.phone_number != phones_list[0] else phones_list[1]
-    contact_created_by_user = Contact.objects.filter(created_by=auth_user, phone_number=result).exists()
-    if desired_value == 'name' and contact_created_by_user:
+    phones_list = list(user_set.values_list("phone_number", flat=True))
+    result = (
+        phones_list[0] if auth_user.phone_number != phones_list[0] else phones_list[1]
+    )
+    contact_created_by_user = Contact.objects.filter(
+        created_by=auth_user, phone_number=result
+    ).exists()
+    if desired_value == "name" and contact_created_by_user:
         contact = Contact.objects.get(created_by=auth_user, phone_number=result)
         return contact.name
-    
-    elif desired_value == 'name' and contact_created_by_user == False:
+
+    elif desired_value == "name" and contact_created_by_user == False:
         return result
-    
-    elif desired_value == 'photo':
+
+    elif desired_value == "photo":
         contact = User.objects.get(phone_number=result)
         if contact.has_photo:
             return contact.photo.url
         else:
-            return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNBNdcMDNS2r9df1IWFVc8AY0QNtfNhEJv7fGS5TdhUWrlBqfGu1PCCn9lKpL-FqF9dWc&usqp=CAU'
+            return "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNBNdcMDNS2r9df1IWFVc8AY0QNtfNhEJv7fGS5TdhUWrlBqfGu1PCCn9lKpL-FqF9dWc&usqp=CAU"
+
 
 @register.simple_tag
 def get_contact_photo(phone):
@@ -52,4 +62,4 @@ def get_contact_photo(phone):
     if user.has_photo:
         return user.photo.url
     else:
-        return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNBNdcMDNS2r9df1IWFVc8AY0QNtfNhEJv7fGS5TdhUWrlBqfGu1PCCn9lKpL-FqF9dWc&usqp=CAU'
+        return "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNBNdcMDNS2r9df1IWFVc8AY0QNtfNhEJv7fGS5TdhUWrlBqfGu1PCCn9lKpL-FqF9dWc&usqp=CAU"
