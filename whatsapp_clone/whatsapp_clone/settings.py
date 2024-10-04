753d73fc1161786da2ed17b328131215ee2cfc8f
franchsli <112600427+franchsli@users.noreply.github.com>
2024-10-04 16:05:32 -0500

    Added render in allowed hosts and changed secret key

--- whatsapp_clone/whatsapp_clone/settings.py
+++ whatsapp_clone/whatsapp_clone/settings.py
@@ -10,12 +10,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^s2fgx-ldzz37ci&z!yel6nk#8$*^^=prpir(=^nbdz*+%_of*'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-^s2fgx-ldzz37ci&z!yel6nk#8$*^^=prpir(=^nbdz*+%_of*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
