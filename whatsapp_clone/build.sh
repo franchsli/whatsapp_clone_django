f28eb62da5a46fcbcad22b77cd804efc84c2320b
franchsli <112600427+franchsli@users.noreply.github.com>
2024-10-10 17:57:04 -0500

    Removed daphne command from build.sh

--- whatsapp_clone/build.sh
+++ whatsapp_clone/build.sh
@@ -10,6 +10,3 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
-
-# run daphne for websockets
-daphne whatsapp_clone.asgi:application
