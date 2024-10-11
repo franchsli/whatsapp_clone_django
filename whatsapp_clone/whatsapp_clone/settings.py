d6547939193f04a5026200d0d933a209515908c9
franchsli <112600427+franchsli@users.noreply.github.com>
2024-10-11 18:58:34 -0500

    Added settings to allow csrf and cors from the deploy page hosting

--- whatsapp_clone/whatsapp_clone/settings.py
+++ whatsapp_clone/whatsapp_clone/settings.py
@@ -21,7 +21,9 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'whatsapp-clone-django.onrender.com']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
+    CSRF_TRUSTED_ORIGINS = ["https://whatsapp-clone-django.onrender.com"]
+    CSRF_ALLOWED_ORIGINS = ["https://whatsapp-clone-django.onrender.com"]
+    CORS_ORIGINS_WHITELIST = ["https://whatsapp-clone-django.onrender.com"]
 
 # Application definition
 
