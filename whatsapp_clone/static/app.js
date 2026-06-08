7c88ef855f942c2bb7f57e865ea369f86e1a01e3
Francisco <112600427+franchsli@users.noreply.github.com>
2026-06-08 15:21:05 -0500

    feat: update contact list adfter form submission

--- whatsapp_clone/static/app.js
+++ whatsapp_clone/static/app.js
@@ -308,15 +308,16 @@ class App {
                 }
             }
         }
-        this.contact_form.onsubmit = async () => {
+        this.contact_form.onsubmit = async (event) => {
             debugger
+            event.preventDefault()
             const contact_form_validation = await tools.validate_contact_form(this.contact_form)
             if (contact_form_validation.is_valid) {
                 tools.create_contact_via_consumer(this.contact_form, this.chat_websocket.client_websocket)
                 tools.triggerNotification('Server', 
                     'The contact was created successfully! Update your contacts list by clicking the "contacts" button.',
                     this.notification_audio)
-                tools.update_chat_list()
+                tools.update_contact_list()
             }
             else {
                 const validation_message_container = document.getElementById('contact-validation-message')
