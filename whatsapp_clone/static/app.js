d20c4d65917e34810c004012b57886aa7d804d31
franchsli <112600427+franchsli@users.noreply.github.com>
2026-09-06 15:34:52 -0500

    style: remove duplicated htmx:beforeRequest eventListener in app.js

--- whatsapp_clone/static/app.js
+++ whatsapp_clone/static/app.js
@@ -660,6 +660,23 @@ document.addEventListener('DOMContentLoaded', () => {
             const messages = document.getElementById('chat-messages')
             window.previousScrollableView = messages.scrollHeight - messages.clientHeight
         }
+        // cancel the request if the requested chats is already displayed.
+        if(event.detail.pathInfo.requestPath.includes('display_chat')){
+            if(main.newMessage){
+                main.newMessage = false
+                return
+            }
+            const displayedChat =  document.getElementById('displayed-chat-info')
+            if(displayedChat){
+                const urlParams = event.detail.pathInfo.requestPath.split('/')
+                const chatId = displayedChat.dataset.displayedChat
+                // if the id of the displayed chat is the same as requested chat id
+                // abort the request
+                if(chatId === urlParams[urlParams.length - 1]){
+                    event.preventDefault()
+                }
+            }
+        }
     })
     htmx.on('htmx:afterSettle', (event) => {
         debugger
@@ -706,25 +723,6 @@ document.addEventListener('DOMContentLoaded', () => {
             const unreadMessagesCounter = document.getElementById(`chat-${sessionStorage.getItem('chatId')}unread-counter`)
             if (unreadMessagesCounter){
                 tools.updateChatList()
-            }
-        }
-    })
-    htmx.on('htmx:beforeRequest', (event) => {
-        // cancel the request if the requested chats is already displayed.
-        if(event.detail.pathInfo.requestPath.includes('display_chat')){
-            if(main.newMessage){
-                main.newMessage = false
-                return
-            }
-            const displayedChat =  document.getElementById('displayed-chat-info')
-            if(displayedChat){
-                const urlParams = event.detail.pathInfo.requestPath.split('/')
-                const chatId = displayedChat.dataset.displayedChat
-                // if the id of the displayed chat is the same as requested chat id
-                // abort the request
-                if(chatId === urlParams[urlParams.length - 1]){
-                    event.preventDefault()
-                }
             }
         }
     })
